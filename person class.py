01db06a81c4696d3c4166d538b81ebb94a424371
d0504140924-max <d0504140924@gmail.com>
2025-09-09 16:56:11 +0300

    update code

--- person class.py
+++ person class.py
@@ -1,8 +1,8 @@
 from datetime import date
 class Person:
-    def __init__(self, p_id, first_name, last_name, age,
+    def __init__(self, id, first_name, last_name, age,
                  birthday, phone_number=None,email=None):
-        assert isinstance(p_id, str)
+        assert isinstance(id, str)
         assert isinstance(first_name, str)
         assert isinstance(last_name, str)
         assert isinstance(age, int)
@@ -16,20 +16,74 @@ class Person:
         self.last_name = last_name
         self.age = age
         self.birthday = birthday
-        self.p_id = p_id
+        self.p_id = id
         self.phone_number = phone_number
         self.email = email
+
+    @property
+    def id(self):
+        return self.id
+    @id.setter
+    def id(self, new_id):
+        self.id = new_id
+
+
+    @property
+    def first_name(self):
+        return self.first_name
+    @first_name.setter
+    def first_name(self, new_name):
+        self.first_name = new_name
+
+
+    @property
+    def last_name(self):
+        return self.last_name
+    @last_name.setter
+    def last_name(self, new_last_name):
+        self.last_name = new_last_name
+
+
+    @property
+    def age(self):
+        return self.age
+    @age.setter
+    def age(self, new_age):
+        self.age = new_age
+
+    @property
+    def all_birthday(self):
+        return (f"{self.birthday['day']:02d}-{self.birthday['month']:02d}-{self.birthday['year']:02d}")
+    @all_birthday.setter
+    def all_birthday(self, new_birthday):
+        self.all_birthday = new_birthday
+
+
+    @property
+    def phone_number(self):
+        return self.phone_number
+    @phone_number.setter
+    def phone_number(self, new_number):
+        self.phone_number = new_number
+
+
+    @property
+    def email(self):
+        return self.email
+    @email.setter
+    def email(self, new_email):
+        self.email = new_email
+
 
 
     @property
     def full_name(self):
         return f'{self.first_name} {self.last_name}'
+
+
     @property
     def contact_information(self):
         return f'{self.phone_number} {self.email}'
-    @property
-    def all_birthday(self):
-        return (f"{self.birthday['day']:02d}-{self.birthday['month']:02d}-{self.birthday['year']:02d}")
 
 
 
