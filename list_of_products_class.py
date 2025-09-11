dddfd2684b32b9cddbf59481dfed264b31b2b430
d0504140924-max <d0504140924@gmail.com>
2025-09-11 15:37:12 +0300

    update code

--- list_of_products_class.py
+++ list_of_products_class.py
@@ -1,27 +1,26 @@
 import json
 import os
-from inventory_class import Inventory
+from inventory_class import InventoryManage
 from product_class import Product
 
 
-class Gson(Inventory):
+class TheInventory(InventoryManage):
 
     _path: str
 
     def __init__(self, path,):
         self.path = path
-        self.items = self.load_from_file()
+
+    @property
+    def items(self):
+        return self.load_from_file()
 
     @property
     def path(self):
         return self._path
-
     @path.setter
     def path(self, new_path):
         self._path = new_path
-
-
-
 
 
     def load_from_file(self):
@@ -31,22 +30,31 @@ class Gson(Inventory):
         return []
 
 
-    def add_item(self, item: Product, mun=1):
-        self.items.append(item.__dict__)
+    def add_item(self, item: Product, num=1):
+        if item.id in self.items:
+            self.items[item.id] += 1
+        else:
+            self.items.append({item.id: num})
+        if not item.__dict__ in self.items:
+            self.items.append(item.__dict__)
         self.save_file()
 
 
-    def remove_item(self, item: Product, mun=1):
-        self.items.remove(item.__dict__)
+    def remove_item(self, item: Product, num=1):
+        if item.id in self.items:
+            for it in range(len(self.items)):
+                if self.items[it][item.id] == 1:
+                    self.items[it].remove()
+                else:
+                    self.items[it][item.id] -= 1
+        if item.__dict__ in self.items:
+            self.items.remove(item.__dict__)
         self.save_file()
 
 
-    def get_amount(self, item: Product):
-        for it in self.items:
-            if it['item'] == item:
-                return it['amount']
-        self.save_file()
-        return None
+
+    def get_amount(self, id_item: Product):
+        return self.items[id_item.id]
 
 
     def update_amount(self, item: Product, new_amount):
